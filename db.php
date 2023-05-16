daa6ef2ed32fe7a9a6d5b289ced109dfa6c0824a
simolorevg <simoneviaggi@libero.it>
2023-05-16 17:35:18 +0200

    Bonus 2 done

--- db.php
+++ db.php
@@ -2,5 +2,6 @@
 require_once __DIR__ . '/models/movie.php';
 $list_of_movies = [
     new Movie('La vita è bella', ['Commedia', 'Dramma'], '1997'),
-    new Movie('2012', ['Fantascienza', 'Catastrofico'], '2009')
+    new Movie('2012', ['Fantascienza', 'Catastrofico'], '2009'),
+    new Movie('Super Size Me', ['Documentario', 'Divulgazione'], '2003')
 ];
--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once __DIR__ . '/db.php';
 <html lang="en">
 
 <head>
+    <!--Bootstrap Link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,13 +15,27 @@ require_once __DIR__ . '/db.php';
 </head>
 
 <body>
-    <h1>Lista Film</h1>
-    <ul>
-        <?php foreach ($list_of_movies as $movie) {
-        ?>
-            <li><?php echo ($movie->printTitle()) ?>. Genere: <?php echo ($movie->printGenere()) ?>. Anno d'uscita: <?php echo ($movie->printYear()) ?></li>
-        <?php } ?>
-    </ul>
+    <div class="container">
+        <h1 class="text-center">Lista Film</h1>
+        <div class="row ">
+            <?php foreach ($list_of_movies as $movie) {
+            ?>
+                <div class="col d-flex justify-content-center align-items-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h3 class="card-title"><?php echo ($movie->printTitle()) ?></h3>
+                            <p class="card-text">
+                            <h4>Genere</h4> <?php echo ($movie->printGenere()) ?>
+                            </p>
+                            <p class="card-text">
+                                <?php echo ($movie->printYear()) ?>
+                            </p>
+                        </div>
+                    </div>
+                <?php } ?>
+                </div>
+        </div>
 </body>
 
 </html>
